c7f61a4192af7a59bde515086ddf4a4d9f7df46f
Carlos Coello <carlos.coello05@outlook.com>
2018-12-12 19:33:14 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItunesService } from './service/itunes.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { Items } from './model/itunesItems';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'itunes-api';
+export class AppComponent implements OnInit {
+  itunesForm: FormGroup;
+  loading = false;
+  notFound = false;
+  items: Items[];
+
+  constructor(private is: ItunesService, private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  ngOnInit() {
+    this.itunesForm.get('term').valueChanges
+    .pipe(debounceTime(600)).subscribe(val => {
+        if (val) {
+          this.notFound = false;
+          this.loading = true;
+          this.is.searchItunes(val).subscribe(data => {
+            setTimeout(() => {
+              this.loading = false;
+              this.items = data;
+              if (!this.items.length) {
+                this.notFound = true;
+              }
+            }, 2000);
+          });
+        }
+    });
+  }
+
+  createForm() {
+    this.itunesForm = this.fb.group({
+      term: ['', Validators.required]
+    });
+  }
+
 }
---
+++ src/app/model/ItunesItems.ts
@@ -0,0 +1,6 @@
+export class Items {
+    constructor(public name: String,
+    public viewUrl: String,
+    public thumbnail: String,
+    public previewSong: String) {}
+}
